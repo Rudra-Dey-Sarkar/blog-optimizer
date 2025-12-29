ac8077d7447ff907440c15acb3725ae4ffaf0852
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-12-29 18:34:55 +0530

    feat(api)/added-CRUD-routes

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { connectDB } from "./configs/db.js";
+import { router } from "./routes/article.js";
 
 connectDB();
 const app = express();
@@ -9,6 +10,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("API is running");
 });
+
+app.use("/articles", router);
 
 app.listen(5000, () => {
     console.log("Server running on port 5000");
