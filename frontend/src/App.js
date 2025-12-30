54ec431b087edd2c11ad8f03961bd507e471b0b9
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-12-30 21:51:27 +0530

    feat(frontend):- initialize frontend with React, Tailwind CSS, and essential configurations

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-neutral-900 text-white flex items-center justify-center">
+      working
+    </div>
+  );
+}
+
+export default App;
